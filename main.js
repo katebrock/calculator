27a06a2006e7d6b1e901482c7ed546588566b4dc
Kate Brock <katherine.marshall.brock@gmail.com>
2015-10-25 17:54:59 -0500

    final

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var answer=0;
 
 function getNumOper(numberOne){
   numberOne.addEventListener("click", function(){               //this function is allowing when a button is clicked
-    num_one=document.getElementById("result").value;            //to be put in the result box, and then when another
+    num_one=Number(document.getElementById("result").value);    //to be put in the result box, and then when another
     document.getElementById("result").value= "";                //button is clicked it stores the OG value.
     operator = numberOne.value;
   });
@@ -17,9 +17,9 @@ function getNumOper(numberOne){
 document.addEventListener("DOMContentLoaded", function(){
 
 
-function clickHandler(event){
-  event.addEventListener("click", function(){
-    document.getElementById('result').value += event.value;
+function clickHandler(event){                                   //this function is saying that when an element id
+  event.addEventListener("click", function(){                   //input is clicked on the value of that input will
+    document.getElementById('result').value += event.value;     //show up in the result box.
   });
 };
 clickHandler(document.getElementById("num1"));
@@ -38,15 +38,15 @@ getNumOper(document.getElementById("multiply"));
 getNumOper(document.getElementById("subtract"));
 getNumOper(document.getElementById("add"));
 
-document.getElementById("equals").addEventListener("click", function(){
-  num_two= document.getElementById("result").value;
-    if (operator ==="/"){
+document.getElementById("equals").addEventListener("click", function(){   //this function is saying that when an
+  num_two= Number(document.getElementById("result").value);               //operator is used to give the result a
+    if (operator ==="/"){                                                 //value
       answer = num_one/num_two;
     }
     else if (operator ==="*"){
-      answer = num_one*num_two;
-    }
-    else if (operator ==="+"){
+      answer = num_one*num_two;                                           //the if and else statements are allowing
+    }                                                                     //the browser to go through each operation
+    else if (operator ==="+"){                                            //until it finds the one you have inputed
       answer = num_one+num_two;
     }
     else if (operator ==="-"){
@@ -56,23 +56,8 @@ document.getElementById("equals").addEventListener("click", function(){
     document.getElementById("result").value = answer;
 })
 
-document.getElementById("clear").addEventListener("click", function(){
-  document.getElementById("result").value = "";
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("clear").addEventListener("click", function(){    //this function is making the clear button
+  document.getElementById("result").value = "";                           //display nothing until an input is given.
+});                                                                       //making it clear with an empty string.
 
 });
